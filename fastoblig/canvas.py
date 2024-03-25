7ea90bd7b4f64d574bef3f075296012dd9e2a657
Patrick Stünkel <webminz@googlemail.com>
2024-03-25 17:29:17 +0100

    functionality to get courses, students, exercises and submissions from canvas

--- fastoblig/canvas.py
+++ fastoblig/canvas.py
@@ -1,26 +1,37 @@
 from __future__ import annotations
+import math
+from zoneinfo import ZoneInfo
+from datetime import datetime, tzinfo
 import logging
 from typing import Any, Literal
 from pydantic import BaseModel
 import os
 import re
 import requests
+from requests.api import get
 
 # Base URL for Canvas LMS
 BASE_URL = "https://hvl.instructure.com/api/v1"
 
+LOCAL_TZ = ZoneInfo("Europe/Oslo")
+
 # Many Canvas endpoints are paginated
 DEFAULT_PAGE_SIZE = 200
 
-_token = None
-if "CANVAS_TOKEN" in os.environ:
-    _token = os.environ["CANVAS_TOKEN"]
+
+def _auth_header():
+    if "CANVAS_TOKEN" in os.environ:
+        return {
+            'Authorization': f"Bearer {os.environ['CANVAS_TOKEN']}"
+        }
+    else:
+        return {}
 
 
 _course_regex = re.compile(r"(\w+\d+)(-.*)? (\d+)([VH]) (.*)")
 
 class Course(BaseModel):
-    id: str 
+    id: int 
     code: str | None
     description: str | None
     semester : Literal["spring"] | Literal["fall"] | None
@@ -36,31 +47,290 @@ class Course(BaseModel):
                 year = int(match.group(3))
                 semester = "spring" if match.group(4) == "V" else "fall"
                 desc = match.group(5)
-                return Course(id=str(json["id"]), code=code, description=desc,
+                return Course(id=json["id"], code=code, description=desc,
                               year=(2000+year), semester=semester) 
             else:
-                return Course(id=str(json['id']), description=json['name'], year=None,
+                return Course(id=json['id'], description=json['name'], year=None,
                               code=None, semester=None)
         else:
-            logging.error("got unexpected json:")
-            logging.error(json)
+            logging.error("unexpected JSON for object 'course'")
+            logging.debug(json)
             return None
+    
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Course):
+            if self.year and self.semester:
+                if other.year and other.semester:
+                    if self.year == other.year:
+                        s1 = 0 if self.semester == "spring" else 1
+                        s2 = 0 if other.semester == "spring" else 1
+                        return s1 < s2 
+                    else:
+                        return self.year < other.year
+                return False 
+
+        return True
+
+
+_studno_regex = re.compile(r"(\d+)@hvl.no")
+
+class Student(BaseModel):
+    id: int
+    student_no: str | None
+    firstname: str 
+    lastname: str 
+    email: str
+
+    @staticmethod
+    def from_json(json: dict[str, Any]) -> Student | None:
+        if "role" and "user" in json:
+            if json["role"] == "StudentEnrollment" and json['user']['sortable_name'] != "Teststudent":
+                user = json["user"]
+                mail = user['login_id']
+                names = user['sortable_name'].split(',')
+                id = int(user['id'])
+                match = _studno_regex.fullmatch(mail)
+                student_no = None
+                if match:
+                    student_no = match.group(1)
+                return Student(id=id, email=mail, student_no=student_no, firstname=names[1].trim(), lastname=names[0].trim())
+        else:
+            logging.error("unexpected json for object 'student'")
+            logging.debug(json)
+        return None
+
+class Exercise(BaseModel):
+    id : int 
+    course: int
+    name: str 
+    content: str 
+    grading : str | None 
+    max_points: float | None
+    deadline: datetime | None 
+    category: str | None 
+    published: bool = True
+    # gruppesett in Canvas
+    submission_category_id: int | None 
+
+    @staticmethod
+    def from_json(course: int, group: str, json: dict[str, Any]) -> Exercise | None:
+        if "id" and "name" in json:
+            id = json['id']
+            name = json['name']
+            content = str(json['description'])
+
+            published = True
+            if 'published' in json:
+                published = json['published']
+
+            grading = None 
+            if 'grading_type' in json:
+                grading = json['grading_type']
+
+            deadline = None 
+            if 'due_at' in json and json['due_at']:
+                deadline = datetime.fromisoformat(json['due_at'][:-1])
+                deadline = deadline.replace(tzinfo=LOCAL_TZ)
+
+            max_points = None
+            if 'points_possible' in json:
+                max_points = json['points_possible']
+
+            submission_category_id = None
+            if 'group_category_id' in json:
+                submission_category_id = json['group_category_id']
+
+            return Exercise(id=id, name=name, content=content, grading=grading, deadline=deadline,
+                            category=group, course=course, max_points=max_points,
+                            submission_category_id=submission_category_id, published=published)
+        else:
+            logging.error("Unexpected JSON format for object 'exercise'")
+            logging.debug(json)
+
+        return None
+
+    def __lt__(self, other: Any):
+        if isinstance(other, Exercise):
+            if self.deadline and other.deadline:
+                return self.deadline < other.deadline
+            elif self.deadline and other.deadline is None:
+                return True 
+            elif self.deadline is None:
+                return False 
+        return True
+
+
+_submission_group_regex = re.compile(r".*\s(\d+)")
+
+class Submission(BaseModel):
+    id: int 
+    exercise: int 
+    content: str | None
+    submission_group_id: int | None
+    submission_group_name: str | None
+    submission_group_no: int | None 
+    members : list[int]
+    state : str = "unsubmitted"
+    submitted_at: datetime | None  
+    graded_at: datetime | None  
+    grade: float | None = None
+    extended_to: datetime | None = None
+    testresult: str | None = None
+    comment: str | None = None
+    feedback: str | None = None
+
+
+    @staticmethod
+    def from_json(exercise: int, json: dict[str, Any]):
+    # submission  objects have 
+    # - id 
+    # - url if submission_type == "online_url" else look in body
+    # - grade can contain complete if workflow_state="graded"
+    # - score -> similar to grade 
+    # - submitted_at 
+    # - user_id -> relevant if single group
+    # - graded_at 
+    # group.id 
+    # group.name
+        if 'id' in json and 'workflow_state' in json and 'submission_type' in json:
+            id = json['id']
+            if json['submission_type'] == 'online_url':
+                content = json['url']
+            else:
+                content = json['body']
+
+            submission_ts = None
+            state = json['workflow_state']
+            if state != "unsubmitted":
+                if json['submitted_at']:
+                    submission_ts = datetime.fromisoformat(json['submitted_at'][:-1])
+                    submission_ts =  submission_ts.replace(tzinfo=LOCAL_TZ)
+
+            grade_ts = None
+            score = None
+            if state == "graded":
+                if json['graded_at']:
+                    grade_ts = datetime.fromisoformat(json['graded_at'][:-1])
+                    grade_ts = grade_ts.replace(tzinfo=LOCAL_TZ)
+                state = json['grade']
+                score = json['score']
+
+
+            users = []
+            submission_group_id = None 
+            submission_group_name : str | None = None
+            submission_group_no :int | None = None
+            if json['group']['id'] is None:
+                users.append(json['user_id'])
+            else:
+                group = json['group']
+                submission_group_id = group['id']
+                submission_group_name = group['name']
+                if submission_group_name:
+                    match = _submission_group_regex.fullmatch(submission_group_name)
+                    if match:
+                        submission_group_no = int(match.group(1))
+
+            return Submission(id=id,
+                              content=content, 
+                              exercise=exercise,
+                              submitted_at=submission_ts, 
+                              state=state,
+                              members=users,
+                              submission_group_id=submission_group_id, 
+                              submission_group_name=submission_group_name,
+                              submission_group_no=submission_group_no,
+                              grade=score,
+                              graded_at=grade_ts)
+
+        else:
+            logging.error("Unexpected JSON content for object 'submission'")
+            logging.debug(json)
+
+        return None
 
 
 def get_courses() -> list[Course]:
     query = f"per_page={DEFAULT_PAGE_SIZE}"
     url = BASE_URL + "/courses" + "?" + query
-    headers = {
-        'Authorization': f"Bearer {_token}"
-    }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=_auth_header())
     if response.status_code == 200:
         data = response.json()
         courses = [Course.from_json(c) for c in data]
-        result : list[Course] = [c for c in courses if c is not None]
+        result : list[Course] = sorted([c for c in courses if c is not None], reverse=True)
         return result
     else:
-        logging.error(f"Error: got unexpected canvas response: {response.status_code}")
-        logging.error(response.content)
+        logging.error(f"Error: got unexpected Canvas response when retrieving courses: {response.status_code}")
+        logging.debug(response.content)
         return []
+
+
+def get_enrollments(course_id: int) -> list[Student]:
+    query = f"per_page={DEFAULT_PAGE_SIZE}"
+    url = BASE_URL + f"/courses/{course_id}/enrollments"  + "?" + query
+    response = requests.get(url, headers=_auth_header())
+    if response.status_code == 200:
+        data = response.json()
+        students = [Student.from_json(c) for c in data]
+        result : list[Student] = [s for s in students if s is not None]
+        return result
+    else:
+        logging.error(f"Got unexpected Canvas response when retrieving enrollments: {response.status_code}")
+        logging.debug(response.content)
+        return []
+
+
+def get_exercises(course_id: int) -> list[Exercise]:
+    query = f"per_page={DEFAULT_PAGE_SIZE}"
+    # first get all groups
+    ass_groups_url = BASE_URL + f"/courses/{course_id}/assignment_groups"  + "?" + query
+    ass_groups_response = requests.get(ass_groups_url, headers=_auth_header())
+    result = []
+    if ass_groups_response.status_code == 200:
+        ass_groups = [(o['id'], o['name']) for o in ass_groups_response.json() ]
+        for (gid, gname) in ass_groups:
+            ass_url = BASE_URL + f"/courses/{course_id}/assignment_groups/{gid}/assignments"
+            ass_response = requests.get(ass_url, headers=_auth_header())
+            if ass_response.status_code == 200:
+                to_add = [Exercise.from_json(course_id, gname, o) for o in ass_response.json()]
+                for e in to_add:
+                    if e is not None:
+                        result.append(e)
+            else:
+                logging.error(f"Got unexpected Canvas result when retrieving assignments: {ass_response.status_code}")
+                logging.debug(ass_response.content)
+    else:
+        logging.error(f"Got unexpected Canvas result when retrieving assignment groups: {ass_groups_response.status_code}")
+        logging.debug(ass_groups_response.content)
+    return result
+
+
+def get_submissions(course_id: int, exercise_id: int) -> list[Submission]:
+    result = []
+    query = f"per_page={DEFAULT_PAGE_SIZE}&grouped=true&include=group"
+    url = BASE_URL + f"/courses/{course_id}/assignments/{exercise_id}/submissions?" + query
+    response = requests.get(url, headers=_auth_header())
+    if response.status_code == 200:
+        subs = [Submission.from_json(exercise_id, o) for o in response.json()]
+        for s in subs:
+            if s is not None:
+                if s.submission_group_id:
+                    group_member_url = BASE_URL + f"/groups/{s.submission_group_id}/users"
+                    group_member_response = requests.get(group_member_url, headers=_auth_header())
+                    if group_member_response.status_code == 200:
+                        for m in group_member_response.json():
+                            s.members.append(m['id'])
+                    else:
+                        logging.error(f"Unexpected Canvas response when retrieving group members: {group_member_response.status_code}")
+                        logging.debug(group_member_response.content)
+                result.append(s)
+    else: 
+        logging.error("Unexpected Canvas response when retrieving submissions")
+        logging.debug(response.content)
+
+
+    return result
+
+
+
 
--- fastoblig/main.py
+++ fastoblig/main.py
@@ -1,14 +1,84 @@
-import sys
+from pathlib import Path
+from typing import Optional
+from typer import Typer
 from dotenv import load_dotenv
-
-print(sys.path)
+from rich.console import Console
+from rich.table import Table
+import canvas
 
 load_dotenv()
 
-import canvas
+app = Typer(no_args_is_help=True)
+console = Console()
 
-if __name__ == "__main__":
+@app.command(name="courses")
+def list_courses():
+    """
+    Shows all courses registered for the current user in the LMS backend.
+    """
     courses = canvas.get_courses()
-    for c in courses:
-        print(f"{c.id}: ({c.code})'{c.description}'")
+    if courses and len(courses) > 0:
+        table = Table("id", "code", "year", "semester", "title")
+        for c in courses:
+            table.add_row(str(c.id), c.code, str(c.year), c.semester, c.description)
+        console.print(table)
+
+
+@app.command(name="students")
+def list_students(course: int, write_csv: Optional[str] = None):
+    """
+    Shows all students that are enrolled in the given course from LMS backedn.
+    """
+    students = canvas.get_enrollments(course)
+    if students and len(students) > 0:
+        file = None
+        if write_csv:
+            file = open(write_csv, "w")
+            file.write("id,student_no,firstname,lastname,email\n")
+        table = Table("id", "student_no", "firstname", "lastname", "email")
+        for s in students:
+            table.add_row(str(s.id), s.student_no, s.firstname, s.lastname, s.email)
+            if file:
+                file.write(str(s.id))
+                file.write(",")
+                file.write(str(s.student_no))
+                file.write(',"')
+                file.write(s.firstname)
+                file.write('","')
+                file.write(s.lastname)
+                file.write('",')
+                file.write(s.email)
+                file.write("\n")
+        if file:
+            file.close()
+        console.print(table)
+
+@app.command("exercises")
+def get_exercises(course: int):
+    """
+    Shows all exercises that are registered for the course in the LMS backend.
+    """
+    exercises = canvas.get_exercises(course)
+    table = Table("id", "name", "group", "deadline", "published" , "grading", "points")
+    for e in sorted(exercises):
+        table.add_row(str(e.id), e.name, e.category, e.deadline.isoformat() if e.deadline is not None else None,
+                      str(e.published), e.grading, str(e.max_points))
+    console.print(table)
+
+
+@app.command("submissions")
+def get_submissions(course: int, exercise: int):
+    """
+    Shows all submissions for the given exercise.
+    """
+    submissions = canvas.get_submissions(course, exercise)
+    table = Table("id", "state", "group", "students", "repo" , "delivered")
+    for s in submissions:
+        table.add_row(str(s.id), s.state, str(s.submission_group_no), str(s.members), s.content, 
+                      s.submitted_at.isoformat() if s.submitted_at is not None else None)
+    console.print(table)
+
+    
+if __name__ == "__main__":
+    app()
 
